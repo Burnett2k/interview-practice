0f7462d6604251f89559244bac0461ffb2397ab0
sawyer burnett <sawyerburnett@gmail.com>
2025-03-17 13:35:44 -0700

    dfs traversal.. more practice

--- grid-dfs-traversal.js
+++ grid-dfs-traversal.js
@@ -7,37 +7,36 @@ const grid = [
   [0, 0, 0, 1, 1],
 ];
 
-const dfs = (x, y, visited) => {
+// orchestrator
+const outerFunction = (grid, x, y) => {
+  const visited = new Set();
+  // do dfs in each adjacent direction until you hit boundaries
+  // keep track of places you've already visited so you don't hit them again
+
+  // visit
+  // traverse up, right, down, left
+  dfs(grid, x, y, visited);
+};
+
+const dfs = (grid, x, y, visited) => {
+  // if we've already been there or we're out of bounds, don't continue
+  const coords = `${x}-${y}`;
   if (
-    x < 0 ||
-    x > grid.length - 1 ||
-    y < 0 ||
-    y > grid[0].length - 1 ||
-    visited.has(`${x},${y}`)
+    !visited.has(coords) &&
+    x >= 0 &&
+    x <= grid.length - 1 &&
+    y >= 0 &&
+    y <= grid[0].length - 1
   ) {
+    console.log(coords);
+    visited.add(coords);
+    dfs(grid, x - 1, y, visited);
+    dfs(grid, x, y + 1, visited);
+    dfs(grid, x + 1, y, visited);
+    dfs(grid, x, y - 1, visited);
+  } else {
     return;
   }
-  console.log(`visited node ${x}, ${y}`);
-  visited.add(`${x},${y}`);
-  dfs(x - 1, y, visited); // up
-  dfs(x + 1, y, visited); // down
-  dfs(x, y - 1, visited); // left
-  dfs(x, y + 1, visited); // right
 };
 
-const outerFunction = () => {
-  let visited = new Set();
-  const row = 0;
-  const col = 0;
-  console.log("starting at", row, col);
-  console.log(
-    "size of grid",
-    `rows = ${grid.length} cols = ${grid[0].length} totalItems = ${
-      grid.length * grid[0].length
-    }`
-  );
-  dfs(0, 0, visited);
-  console.log("# of visited nodes ", visited.size);
-};
-
-console.log(outerFunction(grid));
+console.log(outerFunction(grid, 0, 0));
