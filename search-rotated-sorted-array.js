77cda1fe32a97ce108681fe1b3dd22975e1fccff
sawyer burnett <sawyerburnett@gmail.com>
2025-05-01 07:05:41 -0700

    fix function names

--- search-rotated-sorted-array.js
+++ search-rotated-sorted-array.js
@@ -30,10 +30,10 @@ const search = (nums, t) => {
 
   return -1; // didn't find anything
 };
-console.log(searchRotated([3, 4, 1, 2], 3)); // 0
-console.log(searchRotated([1, 2, 3, 4], 1)); // 0
-console.log(searchRotated([4, 1, 2, 3], 2)); // 2
-console.log(searchRotated([2, 3, 4, 1], 5)); // -1
-console.log(searchRotated([4, 1, 2, 3], 2)); // 2
-console.log(searchRotated([5, 6, 7, 1, 2, 3, 4], 2)); // 4
-console.log(searchRotated([5, 1, 3], 5)); // 0
+console.log(search([3, 4, 1, 2], 3)); // 0
+console.log(search([1, 2, 3, 4], 1)); // 0
+console.log(search([4, 1, 2, 3], 2)); // 2
+console.log(search([2, 3, 4, 1], 5)); // -1
+console.log(search([4, 1, 2, 3], 2)); // 2
+console.log(search([5, 6, 7, 1, 2, 3, 4], 2)); // 4
+console.log(search([5, 1, 3], 5)); // 0
