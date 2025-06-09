7ec5ad2c8626abe30b561503245ec62a61938936
sawyer burnett <sawyerburnett@gmail.com>
2025-06-09 13:48:50 -0700

    redoing LIS from scratch. DP practice

--- longest-increasing-subsequence.js
+++ longest-increasing-subsequence.js
@@ -1,15 +1,17 @@
 let lis = (nums) => {
-  let dp = Array(nums.length).fill(1);
+  const lis = Array(nums.length).fill(1);
 
-  // iterate backwards
+  // loop through backwards
   for (let i = nums.length - 1; i >= 0; i--) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] < nums[j]) {
-        dp[i] = Math.max(dp[i], 1 + dp[j]);
+        lis[i] = Math.max(lis[i], 1 + lis[j]);
       }
     }
   }
-  return Math.max(...dp);
+
+  return Math.max(...lis);
 };
 
-console.log(lis([10, 9, 2, 5, 3, 7, 101, 18])); // should return 4
+console.log(lis([1, 2, 4, 3])); // should return 4
+console.log(lis([10, 9, 2, 5, 3, 7, 101, 18])); // should return 4 [ 2 , 3, 7, 101 ]
